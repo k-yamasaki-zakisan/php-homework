ded46d0a3675a645c81bd53452523a2c5cb2310c
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-06-13 09:32:32 +0900

    閲覧数カウンター制作完了

--- TextCounter/counter.php
+++ TextCounter/counter.php
@@ -1,17 +1,19 @@
 <html>
     <head>
         <title>F5 Counter</title>
+        <!-- 参考サイトhttps://www.javadrive.jp/php/sample/accesscounter/index1.html -->
     </head>
     <body>
         <?php
         $counter_file = "count.txt";
         $counter_lenght = 8;
 
-        $fp = fopen($counter_file, "r+") or die("失敗した"); // count.txtファイルをfopenで開く
+        //対象ファイルの権限を「chmod 777 count.txt」で変更して読み書き実行を許可した
+        $fp = fopen($counter_file, "r+") or die("ファイル読み込み失敗"); // count.txtファイルをfopenで開く
 
         if($fp){
             if(flock($fp, LOCK_EX)){
-                $counter = fget($fp, $counter_lenght);  // fgets関数でcount.txtに書かれたカウント数を読み込む
+                $counter = fgets($fp, $counter_lenght);  // fgets関数でcount.txtに書かれたカウント数を読み込む
                 $counter++; //カウント数の増加
 
                 rewind($fp);
