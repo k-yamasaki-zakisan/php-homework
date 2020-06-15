383aa0ec7cf6eb54b5e515f45d81bbdc70b16a53
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-06-16 08:01:07 +0900

    musqlデータ投稿、保存、表示完了

--- BulletinBoard_sql/board_save.php
+++ BulletinBoard_sql/board_save.php
@@ -4,14 +4,9 @@
         <meta charset="utf-8">
 
         <title>掲示板アプリ</title>
-        <!-- 参考サイト：DB接続 https://www.php.net/manual/ja/pdo.connections.php -->
     </head>
 
     <body>
-        <h1>掲示板一覧</h1>
-        <a href="./board_input.html">投稿フォーム</a>
-        <br><br>
-
         <?php 
         $dsn = "mysql:dbname=board; host=localhost";
         $username = "root";
@@ -21,16 +16,19 @@
         ini_set("display_errors", On);
         error_reporting(E_ALL);
 
+        // prepare, executeのリファクタリングする必要あり（セキュリティ関連）
         try {
             $db = new PDO($dsn, $username, $password, $attr_emulate_prepares);
-        } catch(PODException $e) {
+            $db -> exec('INSERT INTO post SET name="' .$_POST['name'].'",
+                                              email="' .$_POST['email'].'",
+                                              title="' .$_POST['title'].'",
+                                              content="' .$_POST['content'].'"
+                                              ');
+            header("Location: ./index.php");
+        }
+        catch(PODException $e) {
             echo "DB接続失敗" .$e->getMessage() ."\n";
             die();
-        }
-
-        $records = $db ->query('SELECT * FROM post');
-        while ($record = $records -> fetch()) {
-            print($record['name']."\n");
         }
         ?>
     </body>
