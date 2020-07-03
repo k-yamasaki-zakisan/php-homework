692824c07b0cd62ff1e466003bbf314b213dbfeb
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-07-03 17:27:03 +0900

    本番用に情報を追加

--- BulletinBoard_sql/mysql/create_table_board.sql
+++ BulletinBoard_sql/mysql/create_table_board.sql
@@ -13,3 +13,21 @@ https://26gram.com/mysql-show-tables
 https://qiita.com/sakuraya/items/0dd0bb4114e56f42556d
 
 
+
+postgresのテーブル作成
+CREATE TABLE post1 (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    title VARCHAR(50),
+    content TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comment (
+    id SERIAL PRIMARY KEY,
+    reply TEXT NOT NULL,
+    post_id INTEGER,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(post_id) REFERENCES post1(id)
+);
